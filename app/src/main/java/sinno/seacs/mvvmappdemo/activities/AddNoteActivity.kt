e028004210780689d7b4f97294de6832a9fc7f69
artmoras <arturo.moras@sinnodesign.com>
2023-07-05 13:32:09 -0700

    Front End Finished

---
+++ app/src/main/java/sinno/seacs/mvvmappdemo/activities/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package sinno.seacs.mvvmappdemo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sinno.seacs.mvvmappdemo.R
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
