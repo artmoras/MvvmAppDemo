8d3f4a573c0fa97c3336dedf0102113d7772dfe9
artmoras <arturo.moras@sinnodesign.com>
2023-07-05 10:17:07 -0700

    Init Commit

---
+++ app/src/main/java/sinno/seacs/mvvmappdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package sinno.seacs.mvvmappdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
